cd00f6325aeac31884be1df2477d68b740968d0c
アリ <ali@aalharthi.com>
2022-06-09 23:53:05 +0300

    Major update (breaking changes)
    :white_check_mark: PHP v8.0 and above. According to the [Official PHP Website](https://www.php.net/supported-versions.php) Only version 8.0 and above have active support. Therefore, users should upgrade to 8.0 or above.
    :white_check_mark: Service Provider Improvements.
    :white_check_mark: Localization.

--- config/payfort.php
+++ config/payfort.php
@@ -1,11 +1,42 @@
 <?php
 return [
+    /**
+     * Defines wether to activate the Payfort sandbox enviroment or not.
+     */
     'sandbox' => env('PAYFORT_USE_SANDBOX', false),
+
+    /**
+     * The Payfort merchant account identifier.
+     */
     'merchant_identifier' => env('PAYFORT_MERCHANT_IDENTIFIER'),
+
+    /**
+     * The Payfort account access code.
+     */
     'access_code' => env('PAYFORT_ACCESS_CODE'),
+
+    /**
+     * The Payfort account sha type (sha256/sha512).
+     */
     'sha_type' => env('PAYFORT_SHA_TYPE', 'sha256'),
+
+    /**
+     * The Payfort account sha request phrase.
+     */
     'sha_request_phrase' => env('PAYFORT_SHA_REQUEST_PHRASE'),
+
+    /**
+     * The Payfort account sha response phrase.
+     */
     'sha_response_phrase' => env('PAYFORT_SHA_RESPONSE_PHRASE'),
+
+    /**
+     * The default currency for you app. Currency ISO code 3.
+     */
     'currency' => env('PAYFORT_CURRENCY', 'USD'),
+
+    /**
+     * The URL to return after submitting Payfort forms.
+     */
     'return_url' => env('PAYFORT_RETURN_URL', '/')
 ];
